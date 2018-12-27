06c5b779f63707038932660f3e7b8782a7ec50cf
Gustavo Clay <gustavoclay@gmail.com>
2018-12-27 15:33:38 -0200

    curso TypeScript parte 2: Mais técnicas e boas prática Aula: 01 Requisitos não funcionais

--- alurabank/app/js/controllers/NegociacaoController.js
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -1,6 +1,6 @@
 System.register(["../views/index", "../models/index"], function (exports_1, context_1) {
     "use strict";
-    var index_1, index_2, NegociacaoController;
+    var index_1, index_2, NegociacaoController, DiaDaSemana;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [
@@ -22,15 +22,32 @@ System.register(["../views/index", "../models/index"], function (exports_1, cont
                     this._inputValor = $('#valor');
                     this._negociacoesView.update(this._negociacoes);
                 }
+                _isDiaUtil(data) {
+                    return data.getDay() != DiaDaSemana.SABADO && data.getDay() != DiaDaSemana.DOMINGO;
+                }
                 adiciona(event) {
                     event.preventDefault();
-                    const negociacao = new index_2.Negociacao(new Date(this._inputData.val().replace(/-/g, '/')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    let data = new Date(this._inputData.val().replace(/-/g, '/'));
+                    if (!this._isDiaUtil(data)) {
+                        this._mensagemView.update('Só é permitido negociações somente em dias úteis.');
+                        return;
+                    }
+                    const negociacao = new index_2.Negociacao(data, parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
                     this._negociacoes.adiciona(negociacao);
                     this._negociacoesView.update(this._negociacoes);
                     this._mensagemView.update('Negociação adicionada com sucesso.');
                 }
             };
             exports_1("NegociacaoController", NegociacaoController);
+            (function (DiaDaSemana) {
+                DiaDaSemana[DiaDaSemana["DOMINGO"] = 0] = "DOMINGO";
+                DiaDaSemana[DiaDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+                DiaDaSemana[DiaDaSemana["TERCA"] = 2] = "TERCA";
+                DiaDaSemana[DiaDaSemana["QUARTA"] = 3] = "QUARTA";
+                DiaDaSemana[DiaDaSemana["QUINTA"] = 4] = "QUINTA";
+                DiaDaSemana[DiaDaSemana["SEXTA"] = 5] = "SEXTA";
+                DiaDaSemana[DiaDaSemana["SABADO"] = 6] = "SABADO";
+            })(DiaDaSemana || (DiaDaSemana = {}));
         }
     };
 });
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -18,12 +18,27 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
     }
 
+    private _isDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.SABADO && data.getDay() != DiaDaSemana.DOMINGO;
+    }
+
     adiciona(event: Event) {
 
         event.preventDefault();
 
+        /*
+        * Checagem de negociações fora de dias úteis
+        */
+        let data = new Date(this._inputData.val().replace(/-/g, '/'));
+        if (!this._isDiaUtil(data)) {
+
+            this._mensagemView.update('Só é permitido negociações somente em dias úteis.');
+            return
+        }
+
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, '/')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         );
@@ -32,6 +47,11 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso.');
 
-
     }
+}
+
+
+enum DiaDaSemana {
+
+    DOMINGO, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO
 }
