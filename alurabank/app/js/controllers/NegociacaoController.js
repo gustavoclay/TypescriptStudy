a425808f81040cc4f693d6e194e460c1011aa08b
Gustavo Clay <gustavoclay@gmail.com>
2018-12-27 17:57:19 -0200

    curso TypeScript parte 2: Mais técnicas e boas prática Aula: 03 Definindo uma interface para a API Para rodar o servidor, extrair da pasta projeto inicial API dentro de API rodar o comando npm start uri da api localhost:8080/dados

--- alurabank/app/js/controllers/NegociacaoController.js
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -43,6 +43,26 @@ System.register(["../views/index", "../models/index", "../helpers/decorators/dom
                     this._negociacoesView.update(this._negociacoes);
                     this._mensagemView.update('Negociação adicionada com sucesso.');
                 }
+                importaDados() {
+                    function isOK(res) {
+                        if (res.ok) {
+                            return res;
+                        }
+                        else {
+                            throw new Error(res.statusText);
+                        }
+                    }
+                    fetch('http://localhost:8080/dados')
+                        .then(res => isOK(res))
+                        .then(res => res.json())
+                        .then((dados) => {
+                        dados
+                            .map(dado => new index_2.Negociacao(new Date(), dado.vezes, dado.montante))
+                            .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                        this._negociacoesView.update(this._negociacoes);
+                    })
+                        .catch(err => console.log(err.message));
+                }
             };
             __decorate([
                 domInject_1.domInject('#data')
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -52,6 +52,31 @@ export class NegociacaoController {
         this._mensagemView.update('Negociação adicionada com sucesso.');
 
     }
+
+    importaDados() {
+
+        function isOK(res: Response) {
+
+            if (res.ok) {
+                return res;
+            } else {
+                throw new Error(res.statusText);
+            }
+
+        }
+
+        fetch('http://localhost:8080/dados')
+            .then(res => isOK(res))
+            .then(res => res.json())
+            .then((dados: any[]) => {
+                dados
+                    .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                    .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                this._negociacoesView.update(this._negociacoes);
+            })
+            .catch(err => console.log(err.message));
+    }
+
 }
 
 
