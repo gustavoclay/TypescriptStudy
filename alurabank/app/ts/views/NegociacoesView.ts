183016afaa81e6b20a121566753031acf5c13c9b
Gustavo Clay <gustavoclay@gmail.com>
2018-12-27 10:04:44 -0200

    Continuar da 01 Repetindo código...oh my!, Capitulo 04

--- alurabank/app/ts/views/NegociacoesView.ts
+++ alurabank/app/ts/views/NegociacoesView.ts
@@ -7,11 +7,11 @@ class NegociacoesView {
         this._elemento = document.querySelector(seletor);
     }
 
-    update(): void{
-        this._elemento.innerHTML = this.template();
+    update(model: Negociacoes): void {
+        this._elemento.innerHTML = this.template(model);
     }
 
-    template(): string {
+    template(model: Negociacoes): string {
 
         return `
         
@@ -26,6 +26,16 @@ class NegociacoesView {
             </thead>
 
             <tbody>
+                ${model.paraArray().map(negociacao =>
+            `
+                        <tr>
+                            <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>
+                    `
+        ).join('')}
             </tbody>
 
             <tfoot>
